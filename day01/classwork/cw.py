889e8479322c88c778bb3b001c3952c3b2f970e4
Tato-Kotashvili <tatokotashvili77@gmail.com>
2025-10-01 21:04:33 +0400

    second commit

---
+++ day01/classwork/cw.py
@@ -0,0 +1,14 @@
+full_name=('Tato Kotashvili')
+print(full_name)
+
+#2) შექმენით ცვლადები სადაც შეინახავთ თქვენი მშობლების ან ოჯახის წევრების სახელებს, საბოლოოდ კი ისინი სათითაოდ გამოიტანეთ
+mother_full_name='Miranda Kukania'
+father_full_name='Otar Kotashvili'
+print(mother_full_name)
+print(father_full_name)
+
+
+#3) შექმენით 2 ცვლადი, ერთში დაწერთ თვქენ სახელს, მეორეში კი თვქენ ასაკს, საბოლოოდ კი ორივე დაპრინტეთ და გამოიტანეთ
+my_full_name='Tato Kotashvili'
+my_age=14
+print(my_full_name, my_age)
---
+++ day01/homework/hw.py
@@ -0,0 +1,28 @@
+#1)print() ფუნქციის (რამოდენიმეჯერ) გამოყენებით, დაპრინტეთ სამკუთხედი, ნებისმიერი სიმბოლოთი
+print('   *    ')
+print('  ***  ')
+print(' ***** ')
+print('*******') 
+
+#2) გამოიტანეთ თქვენი საყვარელი 5 საჭმელი, ყველა თითო ხაზზე
+print('Khinkal')
+print('Acharuli Khachapuri')
+print('Shawarma')
+print('Mtsvadi')
+print('Ojakhuri')
+
+#3) მარტივი გამოიტანეთ Hello და თქვენი სახელი
+print('Hello Tato')
+
+
+#4) დაპტინტეთ ოთკუთხედი და ოთკუთხედის შუაში დაწერეთ რაიმე ტექსტი
+print('************')
+print('*I love GOA*')
+print('************')
+
+#5) გააკეთეთ მარტივი მენიუ, ყველა საჭმელს მიუწერეთ ფასი (ყველა ახალ ხაზებზე გამოიტანეთ)
+print('Khinkali - 1 lari')
+print('Acharuli Khachapuri - 20 lari')
+print('Shawarma - 15 lari')
+print('Mtsvadi - 12 lari')
+print('Pizza - 25 lari')
