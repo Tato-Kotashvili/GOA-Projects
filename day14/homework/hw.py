55e7e5fc9608727fd7efc12763604c1b4abae835
Tato-Kotashvili <tatokotashvili77@gmail.com>
2025-11-19 21:35:22 +0400

    second commit

---
+++ day14/homework/hw.py
@@ -0,0 +1,31 @@
+for i in range(1,1001):
+     
+    if i % 10 == 0:
+        print(str(i) + " I love you <3")
+
+    elif i % 10 == 1:
+        print(' ' + str(i) + " I love you <3")
+
+    elif i % 10 == 2:
+        print('  ' + str(i) + " I love you <3")
+
+    elif i % 10 == 3:
+        print('   ' + str(i) + " I love you <3")
+
+    elif i % 10 == 4:
+        print('    ' + str(i) + " I love you <3")
+
+    elif i % 10 == 5:
+        print('     ' + str(i) + " I love you <3")
+
+    elif i % 10 == 6:
+        print('    ' + str(i) + " I love you <3")
+
+    elif i % 10 == 7:
+        print('   ' + str(i) + " I love you <3")
+
+    elif i % 10 == 8:
+        print('  ' + str(i) + " I love you <3")
+
+    elif i % 10 == 9:
+        print(' ' + str(i) + " I love you <3")    
