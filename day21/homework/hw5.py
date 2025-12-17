8f71d11bffdf4416f5fcf2a59b42f37586001420
Tato-Kotashvili <tatokotashvili77@gmail.com>
2025-12-17 17:36:25 +0400

    second commit

--- day21/homework/hw5.py
+++ day21/homework/hw5.py
@@ -4,4 +4,13 @@
 
 
 data = [10, 23, 35, 42, 58, 61]
+
+#First way
 print(data[0]+data[3]+data[4])
+
+#Second way
+sum_even = 0
+for i in range(len(data)):
+    if data[i] % 2 == 0:
+        sum_even = sum_even + data[i]
+print(sum_even)
