5e4ee420246900572bf66d06da89aa3bd15f4ef1
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-02-25 18:51:27 +0400

    second commit

---
+++ day42/homework/hw.py
@@ -0,0 +1,54 @@
+# code wars 1
+
+# def remove_char(s):
+#     return s[1:-1]
+
+
+
+# code wars 2
+
+# def double_integer(i):
+#     return i*2
+
+
+
+# code wars 3
+
+# def greet(name):
+#     return "Hello, " + name + " how are you doing today?"
+
+
+
+# code wars 4
+
+# def basic_op(operator, value1, value2):
+#     if operator == '+':
+#         return value1 + value2
+#     elif operator == '-':
+#         return value1 - value2
+#     elif operator == '*':
+#         return value1 * value2
+#     elif operator == '/':
+#         return value1 / value2
+
+
+
+# code wars 5
+
+#     sum = 0
+#     if a == 0:
+#         return 0
+#     else:
+#         for i in range(len(a)):
+#             sum = sum + a[i]
+#         return sum
+
+# code wars 6
+
+
+
+# def simple_multiplication(number) :
+#     if  number % 2 == 0:
+#         return number*8
+#     else:
+#         return number*9
