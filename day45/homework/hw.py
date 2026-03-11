61ea2b301a4a3a5853a586f1a6bd432b4764df18
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-03-11 22:16:47 +0400

    second commit

--- day45/homework/hw.py
+++ day45/homework/hw.py
@@ -1,24 +1,25 @@
 # code wars 1 (gougebari)
 # def accum(st):
 #     new = ''
+#     count = 0
 #     for i in st:
-#         new = new + i.upper() + i.lower() * st.index(i) + '-'
-#     return new[:-1]
+#         new = new + i.upper() + i.lower() * count + '-'
+#         count += 1
+#     return new[:len(new)-1]
 
 # code wars 2
 # def litres(time):
 #     return int(time*0.5)
 
-# code wars 3  (gougebari)
+# code wars 3
 # def to_jaden_case(string):
-#     new = ''
-#     for i in string:
-#         if string.index(i) == 0 or string[string.index(i)-1] == ' ':
-#             new = new + i.upper()
-#         else:
-#             new = new + i
-#     return '"' + new + '"'
-# print(to_jaden_case("join goal oriented academy"))
+    # words = string.split()
+    # new = ''
+    # for word in range(len(words)):
+    #     new = new + words[word].capitalize()
+    #     if word != len(words)-1:
+    #         new = new + ' '
+    # return new
 
 # code wars 4
 # def lovefunc(flower1, flower2):
