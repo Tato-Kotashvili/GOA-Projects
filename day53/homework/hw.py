e2dce5f29a87f4a1af784af08cf19b9f0710b5a3
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-04-03 23:29:36 +0400

    second commit

---
+++ day53/homework/hw.py
@@ -0,0 +1,25 @@
+# code wars 1
+# def maskify(cc):
+#     return "#"*(len(cc)-4) + cc[-4:]
+
+# code wars 2
+# def no_boring_zeros(n):
+#     if n != 0:
+#         return int(str(n).rstrip('0'))
+#     else:
+#         return 0
+
+# code wars 4
+# def number(lines):
+#     new = []
+#     for i in range(len(lines)):
+#         new.append(str(i+1) + ': ' + lines[i])
+#     return new
+
+# code wars 5
+# def distinct(seq):
+#     new = []
+#     for i in range(len(seq)):
+#         if seq[i] not in new:
+#             new.append(seq[i])
+#     return new
