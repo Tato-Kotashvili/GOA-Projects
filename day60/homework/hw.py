719781c04f118e4e750acf2989b7847201331016
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-04-26 23:03:32 +0400

    second commit

---
+++ day60/homework/hw.py
@@ -0,0 +1,22 @@
+# code wars 1
+# def is_valid_walk(walk):
+#     if len(walk) != 10:
+#         return False
+    
+#     x = 0
+#     y = 0
+    
+#     for i in walk:
+#         if i == 'n':
+#             y += 1
+#         elif i == 's':
+#             y -= 1
+#         elif i == 'e':
+#             x += 1
+#         elif i == 'w':
+#             x -= 1
+    
+#     return x == 0 and y == 0
+
+# code wars 2-5
+# ???
