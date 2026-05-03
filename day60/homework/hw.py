169cce812dd7d156833156b93f905dee010d22f8
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-05-03 13:46:34 +0400

    second commit

--- day60/homework/hw.py
+++ day60/homework/hw.py
@@ -18,5 +18,30 @@
     
 #     return x == 0 and y == 0
 
-# code wars 2-5
+# code wars 2
+# def compute_sum(n):
+#     sum = 0
+#     for i in range(1, n + 1):
+#         for j in str(i):
+#             sum += int(j)
+#     return sum
+
+# code wars 3
+# def delete_nth(order,max_e):
+#     result = []
+#     for i in order:
+#         if result.count(i) < max_e:
+#             result.append(i)
+#     return result
+
+# code wars 4
+# def digital_root(n): 
+#     while n > 9:
+#         sum = 0
+#         for i in str(n):
+#             sum += int(i)
+#             n = sum
+#     return n
+
+# code wars 5 
 # ???
---
+++ day62/homework/hw.py
@@ -0,0 +1,14 @@
+# code wars 1
+# ???
+
+# code wars 2
+# def create_phone_number(n):
+#     number = '('
+#     for i in range(len(n)):
+#         if i == 3:
+#             number = number + ') ' + str(n[i])
+#         elif i == 6:
+#             number = number + '-' + str(n[i])
+#         else:
+#             number = number + str(n[i])
+#     return number
