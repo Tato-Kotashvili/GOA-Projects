7a96ea16aece1e2fc03ff550bd0b8cffda76f5c9
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-06-18 14:09:44 +0400

    second commit

---
+++ day74/hw.py
@@ -0,0 +1,33 @@
+# code wars1
+# def break_chocolate(n, m):
+#     if n * m == 1 or n * m == 0:
+#         return 0
+#     return n * m - 1
+
+# code wars2
+# def is_anagram(test, original):
+#     for i in test:
+#         if len(test) != len(original):
+#             return False
+#         elif test.lower().count(i) != original.lower().count(i):
+#             return False
+#     return True
+
+# code wars3
+# def over_the_road(address, n):
+#     return 2 * n + 1 - address
+
+# code wars4
+# def caffeine_buzz(n):
+#     if n % 3 == 0:
+#         if n % 3 == 0 and n % 2 == 0:
+#             if n % 3 == 0 and n % 4 == 0:
+#                 return "Coffee" + "Script"
+#             else:
+#                 return "Java" + "Script"
+#         else:
+#             return "Java"
+#     else:
+#         return "mocha_missing!"
+
+# codewars 5-6 ????
