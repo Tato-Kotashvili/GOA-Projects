89df018348b2d882ca5b703d2b1581041ee54a10
Tato-Kotashvili <tatokotashvili77@gmail.com>
2026-08-08 15:04:52 +0400

    second commit

---
+++ day89/classwork/script1.js
@@ -0,0 +1,18 @@
+// N1
+console.log('Tato'.repeat(15))
+// N2
+console.log("i am going to dubai".replace('dubai','Tbilisi'))
+// N3
+console.log("my friend told me to tell my other friend to come".replaceAll('friend','bro'))
+// N4
+let name
+console.log(name)
+name = 'tato'
+console.log(name)
+// N5
+const ou 
+console.log(ou)
+ou = 8
+console.log(ou)
+ou = 'tato'
+// როდესაც ვქმნით ცვლადს const keyword-ის გამოყენებით მას ვეღარ ვუცვლით მნიშვნელობას
---
+++ day89/homework/script.js
@@ -0,0 +1,63 @@
+// N1
+let text1 = "I like cats"
+console.log(text1.replace('cats','dogs'))
+// N2
+let text2 = 'Hello Goga'
+console.log(text2.replace('Goga','Nika'))
+// N3
+let text3 = 'apple banana apple orange apple'
+console.log(text3.replaceAll('apple','mango'))
+// N4
+let text4 = "JavaScript is easy. JavaScript is fun."
+console.log(text4.replaceAll('JavaScript','Python'))
+// N5
+let text5 = 'JavaScript'
+console.log(text5.slice(0,4))
+// N6
+let text6 = 'Programming'
+console.log(text6.slice(3,7))
+// N7
+let score = 50
+score += 100
+console.log(score)
+// N8
+let number = 10
+number += 5
+console.log(number)
+// N9
+let money = 100
+money -= 30
+console.log(money)
+// N10
+let number2 = 5
+number2 *= 4
+console.log(number2)
+// N11
+let number3 = 100
+number3 /= 5
+console.log(number3)
+// N12
+let balance = 500
+balance -= 100
+balance += 200
+balance -= 50
+balance *= 2
+console.log(balance)
+// N13
+let x = 10
+x ++
+x --
+console.log(x)
+// N14
+let age = 15;
+const country = "Georgia"
+age = 16
+// country = 'Japan'
+console.log(age,country)
+// error ==> const keyword-ით შექმნილი ცვლადს შეცვლა არ შეგვიძლია
+// N15
+let price = 100
+let quantity = 3
+console.log(price*quantity)
+price -= 20
+console.log(price*quantity)
